cc6dc1356b94bdc2932d55ba07b93abad74ea5df
Omar Branez <omar.branez@gmail.com>
2021-04-05 19:02:38 -0400

    commit tree 6038c960e5ee6396aaf29b96897f3f3ad4ea9954

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        #binding.pry
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+    
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT * 
+            FROM pokemon
+            WHERE id = ?
+        SQL
+        db.execute(sql, id).map do |row|
+            # binding.pry
+            Pokemon.new(
+                :id => row[0],
+                :name => row[1],
+                :type => row[2],
+                :db => db
+                )
+        end.first #to actually return it
+    end
 end
